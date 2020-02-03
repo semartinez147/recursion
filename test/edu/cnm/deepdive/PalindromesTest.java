6787ed806830a2394c976982545ff0fb1669d3ea
Samuel Martinez <semartinez147@gmail.com>
2020-02-03 11:47:59 -0700

    Implemented parameterized tests

---
+++ test/edu/cnm/deepdive/PalindromesTest.java
@@ -0,0 +1,23 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class PalindromesTest {
+
+  @ParameterizedTest
+  @CsvFileSource(resources = "/affirmative.csv")
+  void isPalindromeAffirmative(String testCase) {
+    assertTrue(Palindromes.isPalindrome(testCase));
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(resources = "/negative.csv")
+  void isPalindromeNegative(String testCase) {
+      assertFalse(Palindromes.isPalindrome(testCase));
+  }
+}
